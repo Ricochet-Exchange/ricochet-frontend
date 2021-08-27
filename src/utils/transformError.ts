6270d8ce0c9009beaaefdd3b7c6d957f3f64c2c5
Michael Ghen <mike@mikeghen.com>
2021-08-27 12:00:43 -0400

    make error message better

--- src/utils/transformError.ts
+++ src/utils/transformError.ts
@@ -8,5 +8,5 @@ export const transformError = (e?: any) => {
   if (e.code === -32603) {
     return e.data?.message;
   }
-  return 'Operation failed'; 
+  return 'Operation failed. Refresh the page or try again later.'; 
 };
