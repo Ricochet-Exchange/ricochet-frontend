eb0d61e3a46e1501a61d8c95c805b59f420c4947
George <george@abstractpoint.co.uk>
2021-09-29 09:38:32 +0100

    #17 - trim and pad util function

--- src/utils/balances.ts
+++ src/utils/balances.ts
@@ -6,41 +6,46 @@ const negative1 = web3.utils.toBN(-1);
 function fromWei(
   weiInput: string, 
   decimals: number, 
-  options: { pad: boolean, commify: boolean } = { pad: false, commify: false },
+  options: { pad?: boolean, commify?: boolean } = { pad: false, commify: false },
 ) {
-  var wei = web3.utils.toBN(weiInput); // eslint-disable-line
-  var negative = wei.lt(zero); // eslint-disable-line
+  let wei = web3.utils.toBN(weiInput); // eslint-disable-line
+  let negative = wei.lt(zero); // eslint-disable-line
   const base = web3.utils.toBN(10).pow(web3.utils.toBN(decimals));
   const baseLength = decimals;
-
   if (negative) {
     wei = wei.mul(negative1);
   }
-
-  var fraction = wei.mod(base).toString(10); // eslint-disable-line
-
+  let fraction = wei.mod(base).toString(10); // eslint-disable-line
   while (fraction.length < baseLength) {
     fraction = `0${fraction}`;
   }
-
   if (!options.pad) {
     const fractionParts = fraction.match(/^([0-9]*[1-9]|0)(0*)/);
     if (fractionParts) fraction = fractionParts[1]; // eslint-disable-line
   }
-
-  var whole = wei.div(base).toString(10); // eslint-disable-line
-
+  let whole = wei.div(base).toString(10); // eslint-disable-line
   if (options.commify) {
     whole = whole.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   }
-
-  var value = `${whole}${fraction == '0' ? '' : `.${fraction}`}`; // eslint-disable-line
-
+  let value = `${whole}${fraction == '0' ? '' : `.${fraction}`}`; // eslint-disable-line
   if (negative) {
     value = `-${value}`;
   }
-
   return value;
 }
 
-export { fromWei };
+function trimPad(amount: string, decimals: number) {
+  const [int, fract] = amount.split('.');
+  let fractional = fract || '';
+  fractional = fractional.slice(0, decimals);
+  while (fractional.length < decimals) {
+    fractional = `${fractional}0`;
+  }
+  const combined = [int];
+  if (fractional.length > 0) {
+    combined.push(fractional);
+  }
+  return combined.join('.');
+}
+
+export { fromWei, trimPad };
