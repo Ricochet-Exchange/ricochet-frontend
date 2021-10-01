44d64f31b947580a4860f67d0cbd8fafc7c10362
George <george@abstractpoint.co.uk>
2021-10-01 09:47:35 +0100

    #33 Max button unavailable after approve

--- src/components/main/DowngradeForm/__tests__/downgrade-form.test.tsx
+++ src/components/main/DowngradeForm/__tests__/downgrade-form.test.tsx
@@ -43,3 +43,21 @@ test('Max button resets on amount change', async () => {
     fireEvent.change(amountField, {target: {value: '22'}});
     expect(maxButton).toBeEnabled();
 });
+
+test('Max button resets on external amount change', async () => {
+    const { rerender } = render(<DowngradeForm {...props} />)
+    const maxButton = screen.getAllByRole('button').find(each=>each.textContent === 'MAX')
+    if (!maxButton) throw new Error('Max button not found');
+    fireEvent.click(maxButton);
+    expect(maxButton).toBeDisabled();
+
+    rerender(<DowngradeForm {...props} value="0.5" />);
+    expect(maxButton).toBeEnabled();
+});
+
+test('Max button disabled when amount matches balance', async () => {
+    render(<DowngradeForm {...props} value="0.5" balance="0.5"/>)
+    const maxButton = screen.getAllByRole('button').find(each=>each.textContent === 'MAX')
+    if (!maxButton) throw new Error('Max button not found');
+    expect(maxButton).toBeDisabled();
+});
