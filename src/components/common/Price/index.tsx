7502b1f65f736cb6fbbc65c980b19fdb24ca68ab
Joshua Trujillo <Jtriley15@gmail.com>
2021-10-16 16:22:05 -0400

    moved abi to constants, adjusted typings

--- src/components/common/Price/index.tsx
+++ src/components/common/Price/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, HTMLProps } from "react";
 import { AbiItem } from "web3-utils";
-import ABI from "./abi.json";
+import { getSharePriceFromLaunchPad } from "constants/abis";
 import web3 from "utils/web3instance";
 import { usdcxRicExchangeAddress } from "constants/polygon_config";
 import { fromWei } from 'utils/balances'
@@ -11,7 +11,7 @@ type Props = {
 
 // load abi, create contract instance, get price, normalize price, quicc maths, return
 const getPrice = async (): Promise<string> => {
-  const abi: AbiItem = JSON.parse(JSON.stringify(ABI));
+  const abi = (getSharePriceFromLaunchPad as AbiItem);
   const contract = new web3.eth.Contract(abi, usdcxRicExchangeAddress);
   const price = await contract.methods.getSharePrice().call();
   const normalizedPrice = typeof price === "string" ? price : price.toString();
