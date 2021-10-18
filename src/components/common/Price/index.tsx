23ff8098e5a4f422fc398b149911bd9a89dce50e
Joshua Trujillo <Jtriley15@gmail.com>
2021-10-18 09:34:13 -0400

    updated price component

--- src/components/common/Price/index.tsx
+++ src/components/common/Price/index.tsx
@@ -1,30 +1,30 @@
-import { useEffect, useState, HTMLProps } from "react";
-import { AbiItem } from "web3-utils";
-import { getSharePriceFromLaunchPad } from "constants/abis";
-import web3 from "utils/web3instance";
-import { usdcxRicExchangeAddress } from "constants/polygon_config";
-import { fromWei } from 'utils/balances'
+import React from 'react';
+import { AbiItem } from 'web3-utils';
+import { getSharePriceFromLaunchPad } from 'constants/abis';
+import web3 from 'utils/web3instance';
+import { usdcxRicExchangeAddress } from 'constants/polygon_config';
+import { fromWei } from 'utils/balances';
 
 type Props = {
   // any additional props here
-} & HTMLProps<HTMLSpanElement>;
+} & React.HTMLProps<HTMLSpanElement>;
 
 // load abi, create contract instance, get price, normalize price, quicc maths, return
 const getPrice = async (): Promise<string> => {
   const abi = (getSharePriceFromLaunchPad as AbiItem);
   const contract = new web3.eth.Contract(abi, usdcxRicExchangeAddress);
   const price = await contract.methods.getSharePrice().call();
-  const normalizedPrice = typeof price === "string" ? price : price.toString();
-  return fromWei(normalizedPrice, 18)
+  const normalizedPrice = typeof price === 'string' ? price : price.toString();
+  return fromWei(normalizedPrice, 18);
 };
 
 // return null if no price fetched, else return inline element
 export default function Price(props: Props) {
-  const [price, setPrice] = useState("");
+  const [price, setPrice] = React.useState('');
 
-  useEffect(() => {
+  React.useEffect(() => {
     getPrice().then((p) => setPrice(p));
   });
 
-  return <span {...props}>{price !== "" ? price : "-"}</span>;
+  return <span {...props}>{price !== '' ? price : '-'}</span>;
 }
