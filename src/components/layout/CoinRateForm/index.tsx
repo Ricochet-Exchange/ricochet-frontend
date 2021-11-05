9c86ca793f05fcb36d9c00be8c20bbced69bcbf1
Jonathan Chemla <jchemla@iconem.com>
2021-11-05 01:53:18 +0100

    Add a tooltip to warn user about deposit to fix #28
    Add a tooltip to warn user about deposit to fix #28
    Tooltip is only displayed when mouse cursor hovers over the start button and stream is greater than zero.

--- src/components/layout/CoinRateForm/index.tsx
+++ src/components/layout/CoinRateForm/index.tsx
@@ -4,6 +4,7 @@ import React, {
   FC,
 } from 'react';
 import { useTranslation } from 'i18n';
+import ReactTooltip from 'react-tooltip';
 import ButtonNew from '../../common/ButtonNew';
 import { Coin } from '../../../constants/coins';
 import styles from './styles.module.scss';
@@ -38,7 +39,7 @@ export const CoinRateForm: FC<IProps> = ({
       </div>
       <div className={styles.buttons}>
         <div className={styles.start_wrap}>
-          <ButtonNew color="primary" onClick={onClickStart} className={styles.start} disabled={isLoading}>
+          <ButtonNew color="primary" onClick={onClickStart} className={styles.start} disabled={isLoading} data-tip data-for="depositTooltip">
             {t('Start')}
             /
             {t('Edit')}
@@ -47,6 +48,32 @@ export const CoinRateForm: FC<IProps> = ({
         <div className={styles.stop_wrap}>
           <ButtonNew color="secondary" onClick={onClickStop} className={styles.stop} disabled={isLoading}>{t('Stop')}</ButtonNew>
         </div>
+        <div style={{ flexBasis: '100%', height: '0' }}> </div>
+
+        { parseFloat(value) > 0 ? (
+          <ReactTooltip 
+            id="depositTooltip" 
+            place="right" 
+            effect="solid" 
+            multiline
+            className={styles.depositTooltip}
+          >
+            <span
+              className={styles.depositTooltip_span}
+            >
+              Starting this stream will take a security deposit of 
+              <span style={{ fontWeight: 700 }}> 
+                {` ${(parseFloat(value) * 0.05555).toFixed(2)} ${coin} `}
+              </span>
+              from your balance. 
+              The Deposit will be refunded in full when you close the stream or lost if 
+              your balance hits zero with the stream still open.
+
+            </span> 
+          </ReactTooltip>
+        )
+          : null }
+        <div />
       </div>
     </div>
   );
