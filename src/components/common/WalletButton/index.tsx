2cb63fd9d973485ce8bdb524f72c70382e3ca62b
grandzero <bayramutkuuzunlar@gmail.com>
2021-11-15 23:36:08 +0300

    issue: Better header info on mobile #76 resolved

---
+++ src/components/common/WalletButton/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+import { FontIcon, FontIconName } from "components/common/FontIcon";
+import { LocaleKey } from "i18n/utils";
+import ButtonNew from "components/common/ButtonNew";
+import { trimPad } from "utils/balances";
+import styles from "./styles.module.scss";
+
+interface IProps {
+  account: string;
+  ricBalance?: string;
+}
+
+export const WalletButton: FC<IProps> = ({ ricBalance = "", account }) => {
+  return (
+    <ButtonNew className={styles.balance_panel}>
+      <div className={styles.balance}>{`${trimPad(ricBalance, 6)} RIC`}</div>
+      <div className={styles.address}>{account.substring(0, 10)}</div>
+      <div className={styles.icon_wrap}>
+        <FontIcon
+          className={styles.icon}
+          name={FontIconName.RicoUser}
+          size={16}
+        />
+      </div>
+    </ButtonNew>
+  );
+};
--- src/containers/main/HeaderContainer/index.tsx
+++ src/containers/main/HeaderContainer/index.tsx
@@ -1,27 +1,26 @@
-import React, {
-  FC, useCallback, useState,
-} from 'react';
-import { useLocation } from 'react-router-dom';
-import { Routes } from 'constants/routes';
-import { UserSettings } from 'components/layout/UserSettings';
-import Link from 'components/common/Link';
-import { MobileMenu } from 'components/layout/MobileMenu';
-import { useLang } from 'hooks/useLang';
-import ButtonNew from 'components/common/ButtonNew';
-import { useTranslation } from 'i18n';
-import styles from './styles.module.scss';
-import logo from '../../../assets/images/logo.svg';
-import menuImg from '../../../assets/images/menu.svg';
+import React, { FC, useCallback, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Routes } from "constants/routes";
+import { UserSettings } from "components/layout/UserSettings";
+import Link from "components/common/Link";
+import { MobileMenu } from "components/layout/MobileMenu";
+import { useLang } from "hooks/useLang";
+import ButtonNew from "components/common/ButtonNew";
+import { useTranslation } from "i18n";
+import styles from "./styles.module.scss";
+import logo from "../../../assets/images/logo.svg";
+import menuImg from "../../../assets/images/menu.svg";
+import { WalletButton } from "components/common/WalletButton";
 
 interface IProps {
   address: string;
   balance?: string;
 }
 
-export const HeaderContainer:FC<IProps> = ({ address, balance }) => {
+export const HeaderContainer: FC<IProps> = ({ address, balance }) => {
   const location = useLocation();
   const { language, changeLanguage } = useLang();
-  const { t } = useTranslation('main');
+  const { t } = useTranslation("main");
 
   const [isShowMenu, setIsShowMenu] = useState(false);
 
@@ -33,27 +32,29 @@ export const HeaderContainer:FC<IProps> = ({ address, balance }) => {
     <div className={styles.header_wrap}>
       <div className={styles.mob_panel}>
         <div className={styles.logo}>
-          <Link to={Routes.Invest}><img src={logo} alt="Ricochet" /></Link>
+          <Link to={Routes.Invest}>
+            <img src={logo} alt="Ricochet" />
+          </Link>
         </div>
         <div className={styles.links}>
           <Link
             to={Routes.Invest}
-            className={styles.dca_link} 
+            className={styles.dca_link}
             activeClassName={styles.active}
           >
-            <div>{t('Invest')}</div>
+            <div>{t("Invest")}</div>
           </Link>
           <Link
             to={Routes.Wallet}
-            className={styles.upgrade_link} 
+            className={styles.upgrade_link}
             activeClassName={styles.active}
           >
-            <div>{t('Wallet')}</div>
+            <div>{t("Wallet")}</div>
           </Link>
         </div>
         <div className={styles.settings_wrap}>
           <UserSettings
-            className={styles.dot} 
+            className={styles.dot}
             onSelectLanguage={changeLanguage}
             language={language}
             ricBalance={balance}
@@ -67,7 +68,16 @@ export const HeaderContainer:FC<IProps> = ({ address, balance }) => {
         </div>
         {isShowMenu && <MobileMenu closeMenu={toggleMenuMobile} />}
       </div>
-      <div className={styles.mob_head}>{location.pathname === Routes.Wallet ? t('Wallet') : t('Invest')}</div>
+      <div className={styles.mob_head}>
+        {location.pathname === Routes.Wallet ? (
+          t("Wallet")
+        ) : (
+          <>
+            <div>{t("Invest")}</div>{" "}
+            <WalletButton ricBalance={balance} account={address} />
+          </>
+        )}
+      </div>
     </div>
   );
 };
