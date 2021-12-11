b927a108889e691688306b85bfdccf7f6a95e19b
George <george@abstractpoint.co.uk>
2021-12-11 13:38:55 -0300

    Lint fixes

--- src/components/layout/InvestNav/index.tsx
+++ src/components/layout/InvestNav/index.tsx
@@ -5,8 +5,27 @@ import styles from './styles.module.scss';
 
 export const InvestNav = () => (
   <div className={styles.nav_container}>
-    <NavLink className={styles.nav_link} exact activeClassName={styles.nav_link_active} to={Routes.Invest}>Markets</NavLink>
-    <NavLink className={styles.nav_link} activeClassName={styles.nav_link_active} to={Routes.InvestLiquidityMarkets}>Liquidity Markets</NavLink>
-    <NavLink className={styles.nav_link} activeClassName={styles.nav_link_active} to={Routes.InvestLaunchpads}>Launchpads</NavLink>
+    <NavLink 
+      className={styles.nav_link} 
+      exact
+      activeClassName={styles.nav_link_active} 
+      to={Routes.Invest}
+    >
+      Markets
+    </NavLink>
+    <NavLink 
+      className={styles.nav_link} 
+      activeClassName={styles.nav_link_active} 
+      to={Routes.InvestLiquidityMarkets}
+    >
+      Liquidity Markets
+    </NavLink>
+    <NavLink 
+      className={styles.nav_link} 
+      activeClassName={styles.nav_link_active} 
+      to={Routes.InvestLaunchpads}
+    >
+      Launchpads
+    </NavLink>
   </div>
 );
