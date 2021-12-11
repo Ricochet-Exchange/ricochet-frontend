58ba1b398ba70aed63cc47b1be7e2875b420074d
Abstractpoint <abstractpoint@users.noreply.github.com>
2021-12-11 16:13:00 -0300

    LP, Launchpad Pages, Bank Link (#125)
    * Tailwind integration
    
    * Working invest links
    
    * Lint fixes
    
    * Revert "Tailwind integration"
    
    This reverts commit 44eb488a221a00722c057e1ded6451c3027ed505.
    
    * Style and colour fixes
    
    * Lint fixes
    
    * New icons
    
    * Updated fontello icons and added icons to nav menu buttons
    
    * Added bank link with icon
    
    * Renamed routes
    
    * Lint fix

--- src/components/common/FontIcon/index.tsx
+++ src/components/common/FontIcon/index.tsx
@@ -28,6 +28,11 @@ export enum FontIconName {
   ArrowUp = 'icon-up-open',
   Search = 'icon-search',
   Dot = 'icon-dot-3',
+  Rocket = 'icon-rocket',
+  Shuttle = 'icon-space-shuttle',
+  Swap = 'icon-shuffle-1',
+  Loop = 'icon-loop',
+  Bank = 'icon-bank',
 }
 
 const FontIcon: FC<IProps> = ({ name, size = 24, className }) => (
--- src/pages/InvestPage/index.tsx
+++ src/pages/InvestPage/index.tsx
@@ -3,6 +3,10 @@ import { HeaderContainer } from 'containers/main/HeaderContainer';
 import { InvestContainer } from 'containers/main/InvestContainer';
 import { MainLayout } from 'containers/MainLayout';
 import { useShallowSelector } from 'hooks/useShallowSelector';
+import {
+  useRouteMatch,
+} from 'react-router-dom';
+import { RoutesToFlows } from 'constants/flowConfig';
 import React, {
   FC,
 } from 'react';
@@ -16,14 +20,14 @@ const InvestPage: FC<IProps> = () => {
     address,
     balances, 
   } = useShallowSelector(selectMain);
-
+  const match = useRouteMatch();
   return (
     <MainLayout>
       <div className={styles.header}>
         <HeaderContainer balance={balances && balances[RICAddress]} address={address || 'Connecting'} />
       </div>
       <div className={styles.content}>
-        <InvestContainer />
+        <InvestContainer key={match.path} flowConfig={RoutesToFlows[match.path]} />
       </div>
     
     </MainLayout>
