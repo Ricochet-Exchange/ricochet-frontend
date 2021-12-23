9d0681ea96eb521d37bea107e17bd80736b1706d
Mike Ghen <mike@mikeghen.com>
2021-12-23 18:46:14 -0500

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use("/manifest.json", function(req, res, next) {
     res.set({
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Methods": "GET,OPTIONS",
       "Access-Control-Allow-Headers":
         "X-Requested-With, content-type, Authorization",
     })
