75a38bd86eaca9bcc9fd4d91dd9da6698c7aba03
Mehul Soni <65469737+msoni89@users.noreply.github.com>
2021-12-24 18:32:42 +0200

    Fixed cors issue (#158)
    * added proxy in package.json
    
    * cors settings
    
    * added static.json
    
    * removed static.json
    
    * added static.json
    
    * added static.json

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,11 @@
-// App is an express application, we can add an express middleware that will set headers for manifest.json request
-// https://create-react-app.dev/docs/proxying-api-requests-in-development/#configuring-the-proxy-manually
-
 module.exports = function(app) {
-  app.use("/manifest.json", function(req, res, next) {
-    res.set({
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,OPTIONS",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization",
+    app.use("/manifest.json", function(req, res, next) {
+        res.set({
+            "Access-Control-Allow-Origin": "https://gnosis-safe.io",
+            "Access-Control-Allow-Methods": "GET, OPTIONS",
+            "Access-Control-Allow-Headers":
+                "X-Requested-With, content-type, Authorization",
+        })
+        next()
     })
-    next()
-  })
 }
