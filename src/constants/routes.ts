a5f49d1ec48c8fa280e2c63b5c604ab81c5d0fdd
Samir Salem <slmsr90@gmail.com>
2022-01-12 11:44:46 +0100

    fix eslint backtick error

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,10 +1,10 @@
 const home = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : "";
 export const Routes: any = {
-  Invest: `${home}/invest/rex-market`,
-  InvestStreams: `${home}/invest/streams`,
-  InvestLiquidityMarkets: `${home}/invest/rex-lp`,
-  InvestLaunchpads: `${home}/invest/rex-launchpad`,
-  Wallet: `${home}/wallet`,
-  Banks: `${home}/banks`,
-  Vaults: `${home}/vaults`,
+  Invest: "${home}/invest/rex-market",
+  InvestStreams: "${home}/invest/streams",
+  InvestLiquidityMarkets: "${home}/invest/rex-lp",
+  InvestLaunchpads: "${home}/invest/rex-launchpad",
+  Wallet: "${home}/wallet",
+  Banks: "${home}/banks",
+  Vaults: "${home}/vaults",
 };
