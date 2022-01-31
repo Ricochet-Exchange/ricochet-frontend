a0ea25e41e0fcd84e137a8809da450e894186904
MrSquiggles13 <ayresguitar@gmail.com>
2022-01-31 16:45:43 -0500

    Added the Max Priority Fees to the rest of the methods

--- src/api/ethereum.ts
+++ src/api/ethereum.ts
@@ -582,7 +582,7 @@ export const makeDeposit = async (
   let transactionHash;
   const deposit = await bankContract.methods
     .vaultDeposit(amount)
-    .send({ from: accountAddress })
+    .send({ from: accountAddress, maxPriorityFeePerGas: await getSuggestedPriorityGasFee(), })
     .once('transactionHash', (txHash: string) => {
       transactionHash = txHash;
     })
@@ -599,7 +599,7 @@ export const makeBorrow = async (
   let transactionHash;
   const borrow = await bankContract.methods
     .vaultBorrow(amount)
-    .send({ from: accountAddress })
+    .send({ from: accountAddress, maxPriorityFeePerGas: await getSuggestedPriorityGasFee(), })
     .once('transactionHash', (txHash: string) => {
       transactionHash = txHash;
     })
@@ -619,7 +619,7 @@ export const approveToken = async (
     .pow(web3.utils.toBN(255));
   const approveRes = await tokenContract.methods
     .approve(bankAddress, mainWad)
-    .send({ from: accountAddress })
+    .send({ from: accountAddress, maxPriorityFeePerGas: await getSuggestedPriorityGasFee(), })
     .once('transactionHash', (txHash: string) => {
       console.log(txHash);
     })
@@ -637,7 +637,7 @@ export const makeWithdraw = async (
   let transactionHash;
   const whithdraw = await bankContract.methods
     .vaultWithdraw(amount)
-    .send({ from: accountAddress })
+    .send({ from: accountAddress, maxPriorityFeePerGas: await getSuggestedPriorityGasFee(), })
     .once('transactionHash', (txHash: string) => {
       transactionHash = txHash;
     })
@@ -654,7 +654,7 @@ export const makeRepay = async (
   let transactionHash;
   const repay = await bankContract.methods
     .vaultRepay(amount)
-    .send({ from: accountAddress })
+    .send({ from: accountAddress, maxPriorityFeePerGas: await getSuggestedPriorityGasFee(), })
     .once('transactionHash', (txHash: string) => {
       transactionHash = txHash;
     })
