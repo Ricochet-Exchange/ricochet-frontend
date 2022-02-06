cbe739b67ef79d025915c6f8731e7b152ba447ec
sky cherfa <sherifcherfa@gmail.com>
2022-02-06 17:59:36 -0500

    minor fix to padding

--- src/containers/main/ContractContainer/index.tsx
+++ src/containers/main/ContractContainer/index.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.scss';
 
 export const ContractContainer = () => (
   (
-    <div className={styles.container}>
+    <div className={styles.cont_container}>
       <h1 className={styles.main_header}>Network Directory</h1>
       <h5 className={styles.small_header}>Contract addresses on Polygon and more</h5>
       <hr />
