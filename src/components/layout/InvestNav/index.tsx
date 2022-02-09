362315e6b079d2b728f07743ab4ae6fa73784768
bigint <bigint@icloud.com>
2022-02-09 22:13:36 +0530

    chore: improve sidebar (#326)
    * chore: improve sidebar
    
    * chore: fix

--- src/components/layout/InvestNav/index.tsx
+++ src/components/layout/InvestNav/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import cx from 'classnames';
 import { FontIcon, FontIconName } from 'components/common/FontIcon';
 import { NavLink, useLocation } from 'react-router-dom';
 import { selectUserStreams } from 'store/main/selectors';
@@ -17,95 +16,99 @@ export const InvestNav = () => {
   return (
     <div className={styles.nav_container}>
       {userStreams.length > 0 && (
-
-      <NavLink 
-        className={styles.nav_link} 
-        exact
-        activeClassName={styles.nav_link_active} 
-        to={Routes.InvestStreams}
-      >
-        <FontIcon name={FontIconName.RicoUser} size={12} />
-  &nbsp;Streams (
-        {userStreams.length}
-        )
-      </NavLink>
+        <NavLink
+          className={styles.nav_link}
+          exact
+          activeClassName={styles.nav_link_active}
+          to={Routes.InvestStreams}
+        >
+          <FontIcon name={FontIconName.RicoUser} size={16} />
+          <div className={styles.nav_text}>
+            Streams (
+            {userStreams.length}
+            )
+          </div>
+        </NavLink>
       )}
 
-      <Link 
-        to={Routes.Wallet} 
-        className={styles.nav_link_desktop_only} 
+      <Link
+        to={Routes.Wallet}
+        className={styles.nav_link_desktop_only}
         activeClassName={styles.nav_link_active}
       >
-        <img src="https://cdn0.iconfinder.com/data/icons/zondicons/20/wallet-512.png" alt="wallet icon" className={styles.wallet_icon} />
-        &nbsp;Wallet
-        
+        <img
+          src="https://cdn0.iconfinder.com/data/icons/zondicons/20/wallet-512.png"
+          alt="wallet icon"
+          className={styles.wallet_icon}
+        />
+        <div className={styles.nav_text}>Wallet</div>
       </Link>
-     
-      <NavLink 
-        className={styles.nav_link} 
-        exact  
-        activeClassName={styles.nav_link_active} 
+
+      <NavLink
+        className={styles.nav_link}
+        exact
+        activeClassName={styles.nav_link_active}
         to={Routes.Invest}
       >
-        <FontIcon name={FontIconName.Swap} size={12} />
-          &nbsp;Market
+        <FontIcon name={FontIconName.Swap} size={16} />
+        <div className={styles.nav_text}>Market</div>
       </NavLink>
 
-      <NavLink 
-        className={styles.nav_link} 
-        activeClassName={styles.nav_link_active} 
+      <NavLink
+        className={styles.nav_link}
+        activeClassName={styles.nav_link_active}
         to={Routes.InvestLiquidityMarkets}
       >
-        <FontIcon name={FontIconName.Loop} size={12} />
-  &nbsp;Streaming
+        <FontIcon name={FontIconName.Loop} size={16} />
+        <div className={styles.nav_text}>Streaming</div>
       </NavLink>
 
-      <NavLink 
-        className={styles.nav_link} 
-        activeClassName={styles.nav_link_active} 
+      <NavLink
+        className={styles.nav_link}
+        activeClassName={styles.nav_link_active}
         to={Routes.InvestLaunchpads}
       >
-        <FontIcon name={FontIconName.Shuttle} size={12} />
-  &nbsp;Launchpad
+        <FontIcon name={FontIconName.Shuttle} size={16} />
+        <div className={styles.nav_text}>Launchpad</div>
       </NavLink>
-      
+
       <NavLink
-        className={styles.nav_link} 
-        activeClassName={styles.nav_link_active} 
+        className={styles.nav_link}
+        activeClassName={styles.nav_link_active}
         to={Routes.Banks}
       >
-        <FontIcon name={FontIconName.Bank} size={12} />
-  &nbsp;Bank
+        <FontIcon name={FontIconName.Bank} size={16} />
+        <div className={styles.nav_text}>Bank</div>
       </NavLink>
 
-      {(location.pathname === Routes.Banks || location.pathname === Routes.Vaults) && (
-      <NavLink
-        className={cx(styles.nav_link)}
-        activeClassName={styles.nav_link_active}
-        to={Routes.Vaults}
-      >
-        <FontIcon name={FontIconName.Lock} size={16} />
-        &nbsp;Vault
-      </NavLink>
+      {(location.pathname === Routes.Banks ||
+        location.pathname === Routes.Vaults) && (
+        <NavLink
+          className={styles.nav_link}
+          activeClassName={styles.nav_link_active}
+          to={Routes.Vaults}
+        >
+          <FontIcon name={FontIconName.Lock} size={16} />
+          <div className={styles.nav_text}>Vault</div>
+        </NavLink>
       )}
 
-      <NavLink 
-        to={Routes.Refer} 
-        className={styles.nav_link_desktop_only} 
+      <NavLink
+        to={Routes.Refer}
+        className={styles.nav_link_desktop_only}
         activeClassName={styles.nav_link_active}
       >
         <div>Refer</div>
       </NavLink>
-      
+
       <NavLink
-        className={styles.link}
+        className={styles.nav_link}
         to={{ pathname: TUTORIAL_LINK }}
         target="_blank"
       >
-        Tutorial&nbsp;
+        <div className={styles.nav_text_tutorial}>Tutorial</div>
         <FontIcon name={FontIconName.External} size={16} />
       </NavLink>
-       
     </div>
   );
 };
