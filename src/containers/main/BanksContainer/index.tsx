914a258e7d1ca4fda97507c098e3da33dcb8e9de
sky cherfa <sherifcherfa@gmail.com>
2022-02-23 03:54:25 -0500

    final indentation fix

--- src/containers/main/BanksContainer/index.tsx
+++ src/containers/main/BanksContainer/index.tsx
@@ -1,12 +1,14 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { BankDetails } from 'components/banks/BankDetails';
 import { LoadingWrapper } from 'components/common/LoadingWrapper';
 import { selectMain } from 'store/main/selectors';
 import { selectBanks } from 'store/banks/selectors';
 import { useShallowSelector } from 'hooks/useShallowSelector';
+import { SignInButton } from 'components/banks/SignInButton';
 import { BankType } from 'store/banks/types';
 import { useDispatch } from 'react-redux';
 import { banksGetData } from 'store/banks/actionCreators';
+import { LoadingPopUp } from 'components/common/LoadingPopUp';
 import { connectWeb3Modal } from 'store/main/actionCreators';
 import { InvestNav } from 'components/layout/InvestNav';
 import styles from './styles.module.scss';
@@ -15,6 +17,7 @@ export const BanksContainer = () => {
   const dispatch = useDispatch();
   const { banks } = useShallowSelector(selectBanks);
   const { address: accountAddress, isLoading } = useShallowSelector(selectMain);
+  const [hasBanks, setHasBank] = useState(false);
 
   const handleSignIn = useCallback(() => {
     dispatch(connectWeb3Modal());
@@ -23,6 +26,12 @@ export const BanksContainer = () => {
   useEffect(() => {
     if (!isLoading) dispatch(banksGetData());
   }, [isLoading]);
+
+  useEffect(() => {
+    if (banks) {
+      setHasBank(banks.length !== 0);
+    }
+  }, [banks]);
 
   const renderBanks = () =>
     banks.map((bank: BankType) => (
@@ -38,41 +47,62 @@ export const BanksContainer = () => {
     <div className={styles.outer_container}>
       <InvestNav />
       <div className={styles.container}>
-        <LoadingWrapper
-          isLoading={isLoading}
-          className={styles.fullframe}
-        >
-          <table className={styles.dextable}>
-            <thead>
-              <tr>
-                <td className={styles.section}>Name</td>
-                <td className={styles.section}>
-                  Available for
-                  <br />
-                  borrow
-                </td>
-                <td className={styles.section}>
-                  Collateral Price
-                  <br />
-                  in
-                  <span className={styles.blue}> USD</span>
-                </td>
-                <td className={styles.section}>
-                  Debt Price
-                  <br />
-                  in
-                  <span className={styles.blue}> USD</span>
-                </td>
-                <td className={styles.section}>Interest Rate</td>
-                <td className={styles.section}>Origination Fee</td>
-                <td className={styles.section}>Collateralization Ratio</td>
-                <td className={styles.section}>Liquidation Penalty</td>
-                <td>Create Vault</td>
-              </tr>
-            </thead>
-            <tbody>{renderBanks()}</tbody>
-          </table>
-        </LoadingWrapper>
+        {accountAddress ? (
+          <>
+            <LoadingWrapper
+              isLoading={isLoading}
+              className={styles.fullframe}
+            >
+              <div className={styles.contentTotal}>
+                {hasBanks ? (
+                  <table className={styles.dextable}>
+                    <thead>
+                      <tr>
+                        <td className={styles.section}>Name</td>
+                        <td className={styles.section}>
+                          Available for
+                          <br />
+                          borrow
+                        </td>
+                        <td className={styles.section}>
+                          Collateral Price
+                          <br />
+                          in
+                          <span className={styles.blue}> USD</span>
+                        </td>
+                        <td className={styles.section}>
+                          Debt Price
+                          <br />
+                          in
+                          <span className={styles.blue}> USD</span>
+                        </td>
+                        <td className={styles.section}>Interest Rate</td>
+                        <td className={styles.section}>Origination Fee</td>
+                        <td className={styles.section}>Collateralization Ratio</td>
+                        <td className={styles.section}>Liquidation Penalty</td>
+                        <td>Create Vault</td>
+                      </tr>
+                    </thead>
+                    <tbody>{renderBanks()}</tbody>
+                  </table>
+                ) : (
+                  <div className={styles.bank_empty}>
+                    <LoadingPopUp />
+                  </div>
+                )}
+              </div>
+            </LoadingWrapper>
+          </>
+        )
+          : 
+          (
+            <div className={styles.sign_container}>
+              <p>Sign in to see your vaults</p>
+              <SignInButton
+                onClick={handleSignIn}
+              />
+            </div>
+          )}
       </div>
     </div>
   );
