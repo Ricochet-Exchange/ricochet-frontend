d7c6ae72a187d0b639ea4d56627ab2fd0637ca3a
bigint <bigint@icloud.com>
2022-02-25 09:38:31 +0530

    fix: UI freeze

--- src/hooks/useENS.ts
+++ src/hooks/useENS.ts
@@ -1,16 +1,16 @@
-import { getDefaultProvider } from '@ethersproject/providers';
+import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 
 const useENS = (
   address: string | null | undefined,
-): { ensName: string | null, ensAvatar: string | null } => {
+): { ensName: string | null; ensAvatar: string | null } => {
   const [ensName, setENSName] = useState<string | null>(null);
   const [ensAvatar, setENSAvatar] = useState<string | null>(null);
 
   useEffect(() => {
     const resolveENS = async () => {
-      if (address) {
-        const provider = await getDefaultProvider();
+      if (address && ethers.utils.isAddress(address)) {
+        const provider = await ethers.providers.getDefaultProvider();
         const name = await provider.lookupAddress(address);
         if (name) {
           const avatar = await provider.getAvatar(name);
