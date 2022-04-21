4a81714704ca2f31b81750d614ba05dee5988221
Yadong Zhang <zhyd007@gmail.com>
2022-04-21 23:20:29 +0800

    revert: transformError.ts

--- src/utils/transformError.ts
+++ src/utils/transformError.ts
@@ -6,9 +6,6 @@ export const transformError = (e?: any) => {
     return e?.message;
   }
   if (e.code === -32603) {
-    if (e?.data?.message === 'execution reverted: noAffiliates') {
-      return 'Affiliates can not stream';
-    }
     return e.data?.message;
   }
   return 'Operation failed. Refresh the page or try again later.'; 
