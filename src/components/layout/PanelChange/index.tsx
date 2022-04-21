a53695c3d16769f07d98c4885295395737f90c9a
Yadong Zhang <zhyd007@gmail.com>
2022-04-22 05:22:07 +0800

    fix: added a toast for when an affilaite tries to stream. (#613)
    * fix: added a toast for when an affilaite tries to stream.
    
    * fix: prevent affiliate to start a stream.
    
    * revert: transformError.ts
    
    * fix: corrected affiliate judge logic.

--- src/components/layout/PanelChange/index.tsx
+++ src/components/layout/PanelChange/index.tsx
@@ -16,16 +16,19 @@ import ReactTimeAgo from 'react-time-ago';
 import TimeAgo from 'javascript-time-ago';
 import { useTranslation } from 'react-i18next';
 import en from 'javascript-time-ago/locale/en.json';
-import styles from './styles.module.scss';
-import { Coin } from '../../../constants/coins';
+import { getContract } from 'utils/getContract';
+import { rexReferralAddress } from 'constants/polygon_config';
+import { referralABI } from 'constants/abis';
+import { Coin } from 'constants/coins';
+import { FlowTypes } from 'constants/flowConfig';
+import { getShareScaler } from 'utils/getShareScaler';
 import { CoinChange } from '../CoinChange';
 import { CoinBalancePanel } from '../CoinBalancePanel';
 import { CoinRateForm } from '../CoinRateForm';
-import { FlowTypes } from '../../../constants/flowConfig';
 // import Price from '../../common/Price';
 import LpAPY from '../../common/LpAPY';
 import Price from '../../common/Price';
-import { getShareScaler } from '../../../utils/getShareScaler';
+import styles from './styles.module.scss';
 
 TimeAgo.addDefaultLocale(en);
 
@@ -81,17 +84,40 @@ export const PanelChange: FC<IProps> = ({
   receivedSoFar,
 }) => {
   const link = getAddressLink(contractAddress);
-  const { web3 } = useShallowSelector(selectMain);
+  const { web3, address } = useShallowSelector(selectMain);
   const [inputShow, setInputShow] = useState(false);
   const [value, setValue] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [lastDistribution, setLastDistribution] = useState<Date>();
   const [shareScaler, setShareScaler] = useState(1e3);
+  const [isAffiliate, setIsAffiliate] = useState(false);
+  const contract = getContract(rexReferralAddress, referralABI, web3);
   const { t } = useTranslation();
 
   useEffect(() => {
     setIsLoading(mainLoading);
   }, [mainLoading]);
+
+  useEffect(() => {
+    let mounted = true;
+
+    if (address && contract) {
+      contract.methods.addressToAffiliate(address.toLowerCase()).call()
+        .then((affiliateId: string) => contract.methods.affiliates(affiliateId).call())
+        .then((res: any) => {
+          if (web3.utils.toBN(res.addr).isZero()) {
+            return;
+          }
+          if (mounted && res.enabled) {
+            setIsAffiliate(true);
+          }
+        });
+    }
+
+    return () => {
+      mounted = false;
+    };
+  }, [address, contract]);
 
   useEffect(() => {
     let isMounted = true;
@@ -148,6 +174,10 @@ export const PanelChange: FC<IProps> = ({
   };
 
   const handleStart = useCallback(() => {
+    if (isAffiliate) {
+      showErrorToast('Affiliates can not stream', 'Error');
+      return;
+    }
     if (Number(balanceA) <= 0 || Number(value) < 0) {
       return;
     }
