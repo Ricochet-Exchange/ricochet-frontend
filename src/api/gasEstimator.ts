822de1b8563a70a5c976b6c6eed5311599ba4060
Mehul Soni <mehul.soni.estonia@gmail.com>
2022-05-22 19:28:58 +0300

    shift to sdk-core and ethereum.ts methods changed, gas fixed

--- src/api/gasEstimator.ts
+++ src/api/gasEstimator.ts
@@ -1,8 +1,9 @@
 import { ethers } from 'ethers';
 
+const GAS_STATION_MATIC_URL = 'https://gasstation-mainnet.matic.network/v2';
 export const gas = async () => {
   try {
-    const gasResponse = await fetch('https://gasstation-mainnet.matic.network/v2')
+    const gasResponse = await fetch(GAS_STATION_MATIC_URL)
       .then(async (response) => response.json())
       .catch(() => ({}));
     const { maxPriorityFee, maxFee } = (gasResponse && gasResponse.fast) || {};
