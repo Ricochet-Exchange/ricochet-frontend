48f0f7a242e5f0a7eaaca7e558179be3ddbceef8
Yadong Zhang <zhyd007@gmail.com>
2022-06-10 00:57:56 +0800

    fix: pulled missing markets prices. (#719)
    * fix: pulled WBTC-DAI price.
    
    * fix: pulled MATIC-USDC, MATIC-DAI prices from quick swap.

--- src/components/common/Price/index.tsx
+++ src/components/common/Price/index.tsx
@@ -9,9 +9,9 @@ import { useShallowSelector } from '../../../hooks/useShallowSelector';
 import { selectMain } from '../../../store/main/selectors';
 import { FlowTypes } from 'constants/flowConfig';
 import { Coin } from 'constants/coins';
-import { querySushiPoolPrices } from 'api';
+import { queryQuickSwapPoolPrices, querySushiPoolPrices } from 'api';
 
-// Sushi Pools
+// SushiSwap Pools
 const poolAddresses = {
 	[`${Coin.RIC}-${Coin.USDC}`]: '0xdbf5d66d77a83b96763c965d193d0fdd1f8a184b', // need {token1Price}
 	[`${Coin.USDC}-${Coin.RIC}`]: '0xdbf5d66d77a83b96763c965d193d0fdd1f8a184b', // need {token0Price}
@@ -21,6 +21,16 @@ const poolAddresses = {
 	[`${Coin.ETH}-${Coin.USDC}`]: '0x34965ba0ac2451A34a0471F04CCa3F990b8dea27',
 	[`${Coin.DAI}-${Coin.ETH}`]: '0x6FF62bfb8c12109E8000935A6De54daD83a4f39f',
 	[`${Coin.ETH}-${Coin.DAI}`]: '0x6FF62bfb8c12109E8000935A6De54daD83a4f39f',
+	[`${Coin.WBTC}-${Coin.DAI}`]: '0x7a1d5E67c3a273274766E241363E3E98e721E456',
+	[`${Coin.DAI}-${Coin.WBTC}`]: '0x7a1d5E67c3a273274766E241363E3E98e721E456',
+};
+
+// QuickSwap Pools
+const quickSwapPools = {
+	[`${Coin.MATIC}-${Coin.USDC}`]: '0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827',
+	[`${Coin.USDC}-${Coin.MATIC}`]: '0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827',
+	[`${Coin.MATIC}-${Coin.DAI}`]: '0xEEf611894CeaE652979C9D0DaE1dEb597790C6eE',
+	[`${Coin.DAI}-${Coin.MATIC}`]: '0xEEf611894CeaE652979C9D0DaE1dEb597790C6eE',
 };
 
 type Props = {
@@ -56,6 +66,31 @@ export default function Price({ flowType, coinA, coinB }: Props) {
 		});
 
 		querySushiPoolPrices(poolAddresses[`${coinA}-${coinB}`]).then(({ data }) => {
+			if (data?.error) {
+				console.error('fetching Sushi Pools price error: ', data.error);
+			} else {
+				const { pair } = data.data;
+				if (isMounted && pair) {
+					const { symbol: _coinA } = pair.token0;
+					const { symbol: _coinB } = pair.token1;
+
+					let realPrice = '';
+					if (_coinA.includes(coinA) && _coinB.includes(coinB)) {
+						realPrice = pair.token0Price;
+					} else if (_coinA.includes(coinB) && _coinB.includes(coinA)) {
+						realPrice = pair.token1Price;
+					}
+					setMarketPairPrice((prev) => {
+						return {
+							...prev,
+							[`${coinA}-${coinB}`]: realPrice,
+						};
+					});
+				}
+			}
+		});
+
+		queryQuickSwapPoolPrices(quickSwapPools[`${coinA}-${coinB}`]).then(({ data }) => {
 			if (data?.error) {
 				console.error('fetching Sushi Pools price error: ', data.error);
 			} else {
