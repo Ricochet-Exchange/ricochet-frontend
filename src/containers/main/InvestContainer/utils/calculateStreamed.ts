60d3a3f8c6e49cb999df90e72f00d82e95ca1226
Yadong Zhang <zhyd007@gmail.com>
2022-06-25 13:08:57 +0800

    fix: streamed.

--- src/containers/main/InvestContainer/utils/calculateStreamed.ts
+++ src/containers/main/InvestContainer/utils/calculateStreamed.ts
@@ -1,20 +1,18 @@
 import { BigNumber, ethers } from 'ethers';
 
-export const calculateStreamed = (balance: string, balanceTimestamp: string, flowRate: string) => {
-	const balanceBN = BigNumber.from(balance);
+export const calculateStreamed = (streamedUntilUpdatedAt: string, balanceTimestamp: string, flowRate: string) => {
+	const streamedUntilUpdatedAtBN = BigNumber.from(streamedUntilUpdatedAt);
 	const balanceTimestampBN = BigNumber.from(balanceTimestamp).mul(1e3);
 	const currentTimestampBN = BigNumber.from(new Date().getTime());
 	const flowRateBN = BigNumber.from(flowRate);
 
-	let streamedSoFar;
+	let streamed;
 
 	if (flowRateBN.isZero()) {
-		streamedSoFar = ethers.utils.formatEther(balanceBN);
+		streamed = ethers.utils.formatEther(streamedUntilUpdatedAtBN);
 	} else {
-		streamedSoFar = ethers.utils.formatEther(
-			balanceBN.add(currentTimestampBN.sub(balanceTimestampBN)).mul(flowRateBN).div(1e3),
-		);
+		streamed = ethers.utils.formatEther(currentTimestampBN.sub(balanceTimestampBN).mul(flowRateBN).div(1e3));
 	}
 
-	return streamedSoFar;
+	return streamed;
 };
