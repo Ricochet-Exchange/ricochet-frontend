c93a1b6ce1f2d479aac0a88c6771de2a121ef656
Mehul Soni <mehul.soni.estonia@gmail.com>
2022-07-30 01:13:09 +0300

    fixed ledger code, ref..

--- src/utils/ledgerhq-frame-connector/helpers.ts
+++ src/utils/ledgerhq-frame-connector/helpers.ts
@@ -1,24 +1,40 @@
+import { THIRTY_DAYS_DURATION } from '../../constants/referralExpiry';
+
 export const isIframe = (): boolean => {
-  try {
-    return window.self !== window.top;
-  } catch (error) {
-    return false;
-  }
+	try {
+		return window.self !== window.top;
+	} catch (error) {
+		return false;
+	}
+};
+const params = new URLSearchParams(window.self.location.search);
+export const isLedgerDappBrowserProvider = (() => {
+	let state: boolean | null = null;
+
+	return (): boolean => {
+		if (typeof state === 'boolean') return state;
+		if (typeof window === 'undefined') return false;
+		try {
+			const isEmbed = !!params.get('embed');
+			state = isIframe() && isEmbed;
+		} catch (error) {
+			state = false;
+		}
+		return !!state;
+	};
+})();
+
+export const isLedgerReferralIdSetIntoCookies = () => {
+	const params = new URLSearchParams(window.self.location.search);
+	const referralId = params.get('ref');
+	if (referralId === 'ledger') {
+		const expires = new Date(new Date().getTime() + THIRTY_DAYS_DURATION);
+		setCookie('referralId', referralId, expires);
+		return true;
+	}
+	return false;
 };
 
-export const isLedgerDappBrowserProvider = (() => {
-  let state: boolean | null = null;
-
-  return (): boolean => {
-    if (typeof state === 'boolean') return state;
-    if (typeof window === 'undefined') return false;
-    try {
-      const params = new URLSearchParams(window.self.location.search);
-      const isEmbed = !!params.get('embed');
-      state = isIframe() && isEmbed;
-    } catch (error) {
-      state = false;
-    }
-    return !!state;
-  };
-})();
+function setCookie(cname: string, cvalue: string, expires: Date) {
+	document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+}
