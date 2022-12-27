ab00ede45d963ac5f7b1b4a239a4d7d94ae8bca6
sky cherfa <sherifcherfa@gmail.com>
2022-12-27 12:12:06 -0500

    remove all unused bank code

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -4,22 +4,20 @@ import storage from 'redux-persist/lib/storage'; // defaults to localStorage for
 import { connectRouter } from 'connected-react-router';
 import main from 'store/main';
 import modal from 'store/modal';
-import banks from 'store/banks';
 import history from 'utils/history';
 import distributions from './distributions';
 
 const PERSIST_CONFIGS: Record<string, PersistConfig<any>> = {
-  main: {
-    storage,
-    key: 'main',
-    whitelist: ['main'],
-  },
-}; 
+	main: {
+		storage,
+		key: 'main',
+		whitelist: ['main'],
+	},
+};
 
 export default combineReducers({
-  main: persistReducer(PERSIST_CONFIGS.main, main),
-  modal,
-  banks,
-  distributions,
-  router: connectRouter(history),
+	main: persistReducer(PERSIST_CONFIGS.main, main),
+	modal,
+	distributions,
+	router: connectRouter(history),
 });
