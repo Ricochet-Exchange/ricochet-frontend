a8678d88e91c2532dfc9b78c613dcfc9943e951a
finch-theworst <105673607+finch-theworst@users.noreply.github.com>
2023-01-11 22:52:57 +0100

    Added main.module.scss file (#847)
    * Added main.module.scss file
    deleted styles.module.scss
    On branch kenichi6336/dw-132/reduce-use-of-stylesheet
    
    * Added a styles.module.scss file in \assets
    with the complied styles
    And returned the main.scss file
    
    * //'InvestNav' is declared but its value is never read.

--- src/components/layout/Card/index.tsx
+++ src/components/layout/Card/index.tsx
@@ -4,24 +4,20 @@ import { LoadingWrapper } from 'components/common/LoadingWrapper';
 import styles from './styles.module.scss';
 
 type Props = {
-  title?: string | JSX.Element
-  main?: boolean;
-  children?: JSX.Element;
-  isLoading?: boolean;
+	title?: string | JSX.Element;
+	main?: boolean;
+	children?: JSX.Element;
+	isLoading?: boolean;
 };
 
-export const Card:React.FC<Props> = ({
-  title, main, children, isLoading, 
-}) => (
-  <div className={cx(styles.card, { [styles.main]: main })}>
-    <LoadingWrapper isLoading={isLoading}>
-      <div className={styles.content}>
-        <div className={styles.title}>
-          {title}
-        </div>
-        <hr className={styles.line} />
-        {children}
-      </div>
-    </LoadingWrapper>
-  </div>
+export const Card: React.FC<Props> = ({ title, main, children, isLoading }) => (
+	<div className={cx(styles.card, { [styles.main]: main })}>
+		<LoadingWrapper isLoading={isLoading}>
+			<div className={styles.content}>
+				<div className={styles.title}>{title}</div>
+				<hr className={styles.line} />
+				{children}
+			</div>
+		</LoadingWrapper>
+	</div>
 );
