8183130959dcd8d1ae4c8e7c5905d546a4b124bf
mikeghen <mike@mikeghen.com>
2023-04-17 10:45:47 -0400

    change links, add V3 warning banner

--- src/containers/MainLayout/index.tsx
+++ src/containers/MainLayout/index.tsx
@@ -3,6 +3,7 @@ import { useShallowSelector } from 'hooks/useShallowSelector';
 import { selectMain } from 'store/main/selectors';
 import { HeaderContainer } from 'containers/main/HeaderContainer';
 import { InvestNav } from 'components/layout/InvestNav';
+import { Banner } from 'components/layout/Banner';
 import { RICAddress } from 'constants/polygon_config';
 import styles from './styles.module.scss';
 
@@ -14,13 +15,13 @@ export const MainLayout: React.FC<IProps> = ({ children }) => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
+				<Banner />
 				<HeaderContainer balance={balances && balances[RICAddress]} address={address || 'Connect Wallet'} />
 			</div>
 
 			<div className={styles.navbar}>
 				<InvestNav />
 			</div>
-
 			<div className={styles.content}>{children}</div>
 		</div>
 	);
