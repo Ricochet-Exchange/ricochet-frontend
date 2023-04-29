a4ce59c3ee2e0bfd1730eee0d63e8e4aff0aeac4
Abhishek Naidu <geekyabhi12@gmail.com>
2023-04-29 11:25:07 +0530

    chore: remove more things

--- src/i18n/index.ts
+++
@@ -1,8 +0,0 @@
-import { addResource, LocaleKey, useTranslation } from './utils';
-import en from './en';
-import de from './de';
-
-addResource(LocaleKey.en, en);
-addResource(LocaleKey.de, de);
-
-export { useTranslation };
